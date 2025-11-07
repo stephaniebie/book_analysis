21dde8a2e034509f5bdd882b09cd682983b5b740
Stephanie Bie <stephaniehbie@gmail.com>
2025-11-07 00:43:16 -0700

    Blueprint tests

---
+++ tests/test_parser.py
@@ -0,0 +1,17 @@
+from book_analysis.parser import (
+    convert_roman_numerals,
+    parse_title,
+    read_txt,
+)
+
+
+def test_read_txt():
+    assert True
+
+
+def test_convert_roman_numerals():
+    assert True
+
+
+def test_parse_title():
+    assert True
---
+++ tests/test_toc.py
@@ -0,0 +1,22 @@
+from book_analysis.toc import (
+    construct_toc,
+    read_toc,
+    Section,
+    TableOfContents,
+)
+
+
+def test_Section():
+    assert True
+
+
+def test_TableOfContents():
+    assert True
+
+
+def test_construct_toc():
+    assert True
+
+
+def test_read_toc():
+    assert True
